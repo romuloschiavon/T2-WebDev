a9c81380af861c0a13dfbfbf14f94b64f4b46b85
CodeWracker <dev.rodrigofs@gmail.com>
2022-12-19 14:38:46 -0300

    login e register

--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -1,20 +1,19 @@
 <template>
-    <div class="signup-container">
-        <div class="img-container">
-            <img alt="UFSC logo" src="../assets/logo.png" class="img-logo-ufsc" />
-        </div  >
+    <div class="form-page-container signup-container">
+        <div class="form-container">
         <h1 class="signup-title">Sign Up</h1>
-        <div class="signup-form-container">
             <form @submit.prevent="signup" class="signup-form">
                 <input id="email" v-model="email" type="email" required placeholder="E-mail">
                 <input id="password" v-model="password" type="password" required placeholder="Password">
-                <input id="password_confirmation" v-model="confirmPassword" type="password" required placeholder="Confirm Password">
+                <input id="password_confirmation" v-model="confirmPassword" type="password" required
+                    placeholder="Confirm Password">
                 <button class="signup-btn" v-on:click="signup">Sign Up</button>
                 <p>
-                    <router-link to="/login" scroll-behavior="smooth">Already have an account? Login</router-link>
+                    <router-link to="/login" class="link-to-login" scroll-behavior="smooth">Already have an account? Login</router-link>
                 </p>
             </form>
         </div>
+        <img alt="UFSC logo" src="../assets/logo.png" class="imagestyle" />
     </div>
 </template>
   
@@ -77,99 +76,17 @@ export default {
   
   
 <style scoped>
-.signup-form-container {
-    width: 100%;
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-.signup-btn{
-    width: 50%;
-    background-color: #4CAF50;
-    border-color: #4CAF50;
-    color: white;
+
+
+.link-to-login {
+    font-family: 'Ubuntu', sans-serif;
+    text-decoration: none;
+    color: #353531;
     cursor: pointer;
 }
-.signup-title{
-    text-align: center;
-    width: 100%;
-}
-form {
-    display: flex;
-    flex-direction: column;
-}
-.img-container{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-}
-.signup-form{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    align-self: center;
-}
-label {
-    margin-bottom: 5px;
-}
-@media screen and (min-width: 768px) {
-    .signup-container{
-        width: 50%;
-    }
-    .img-logo-ufsc {
-        width: 35%;
-    }
-}
-
-@media screen and (max-width: 768px) {
-    .signup-container{
-        width: 85%;
-    }
-    .img-logo-ufsc {
-        width: 100%;
-    }
-}
-.signup-container{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-input {
-    width: 100%;
-    height: 40px;
-    padding-left: 20px;
-    display: block;
-    margin-bottom: 30px;
-    margin-right: auto;
-    margin-left: auto;
-    border: 1px solid skyblue;
-}
-
-button {
-    border: 1px solid skyblue;
-    background: skyblue;
-    width: 50%;
-    height: 40px;
-    color: white;
-    cursor: pointer;
-    margin: 5px;
-    text-align: center;
-    border-radius: 5px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    font-size: 18px;
-}
-
-button:active {
-    opacity: 0.8;
-}
-.login-link{
-    text-align: center;
-    width: 90%;
+.link-to-login:hover {
+    color: #000;
+    text-decoration: underline;
 }
 </style>
   
